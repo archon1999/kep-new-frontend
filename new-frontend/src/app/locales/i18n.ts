dbeda650a6976ace59df9e4c7a8e7124e043d755
Nazarbek Baltabaev <63235428+archon1999@users.noreply.github.com>
2025-11-21 16:01:59 +0500

    Refactor i18n locale files into language folders

--- new-frontend/src/app/locales/i18n.ts
+++ new-frontend/src/app/locales/i18n.ts
@@ -1,18 +1,18 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationEn from './langs/en.json';
-import translationRu from './langs/ru.json';
-import translationUz from './langs/uz.json';
+import enTranslation from './langs/en';
+import ruTranslation from './langs/ru';
+import uzTranslation from './langs/uz';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      enUS: { translation: translationEn },
-      ruRU: { translation: translationRu },
-      uzUZ: { translation: translationUz },
+      enUS: { translation: enTranslation },
+      ruRU: { translation: ruTranslation },
+      uzUZ: { translation: uzTranslation },
     },
     lng: 'enUS',
     ns: ['translation'],
---
+++ new-frontend/src/app/locales/langs/en/index.ts
@@ -0,0 +1,33 @@
+import arena from './arena.json';
+import calendar from './calendar.json';
+import challenges from './challenges.json';
+import common from './common.json';
+import hackathons from './hackathons.json';
+import homePage from './home-page.json';
+import kepcoinPage from './kepcoin-page.json';
+import menu from './menu.json';
+import pageTitles from './page-titles.json';
+import projects from './projects.json';
+import settings from './settings.json';
+import shop from './shop.json';
+import tests from './tests.json';
+import users from './users.json';
+
+export const enTranslation = {
+  ...common,
+  ...menu,
+  ...pageTitles,
+  ...settings,
+  ...arena,
+  ...homePage,
+  ...shop,
+  ...calendar,
+  ...hackathons,
+  ...projects,
+  ...tests,
+  ...users,
+  ...kepcoinPage,
+  ...challenges,
+};
+
+export default enTranslation;
