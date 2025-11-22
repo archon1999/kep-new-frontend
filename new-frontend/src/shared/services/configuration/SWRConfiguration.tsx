2b16db49d48ac6058c66683a004037a6518ed1e9
archon1999 <nazarbek.baltabaev@gmail.com>
2025-11-22 22:22:10 +0500

    SWR config ttl

--- new-frontend/src/shared/services/configuration/SWRConfiguration.tsx
+++ new-frontend/src/shared/services/configuration/SWRConfiguration.tsx
@@ -2,6 +2,39 @@ import { PropsWithChildren } from 'react';
 import axiosFetcher from 'shared/services/axios/axiosFetcher';
 import { SWRConfig } from 'swr';
 
+
+function createTTLCache(ttlMs: number) {
+  const map = new Map();
+
+  return {
+    get(key: any) {
+      const item = map.get(key);
+      if (!item) return undefined;
+
+      const { value, expiresAt } = item;
+
+      if (Date.now() > expiresAt) {
+        map.delete(key);
+        return undefined;
+      }
+
+      return value;
+    },
+    set(key: any, value: any) {
+      map.set(key, {
+        value,
+        expiresAt: Date.now() + ttlMs,
+      });
+    },
+    delete(key: any) {
+      map.delete(key);
+    },
+    keys() {
+      return map.keys();
+    }
+  };
+}
+
 const SWRConfiguration = ({ children }: PropsWithChildren) => {
   return (
     <SWRConfig
@@ -11,6 +44,7 @@ const SWRConfiguration = ({ children }: PropsWithChildren) => {
         revalidateOnFocus: false,
         revalidateOnReconnect: false,
         shouldRetryOnError: false,
+        provider: () => createTTLCache(1000 * 60)
       }}
     >
       {children}
